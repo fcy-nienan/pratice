fa86523a6b36d93bce3eadffeaa891bab9394bec
fcy <807715333@qq.com>
2019-09-09 01:13:47 +0800

    0

--- src/main/java/link/deleteDuplicates.java
+++ src/main/java/link/deleteDuplicates.java
@@ -1,26 +1,40 @@
 package link;
-//输入: 1->2->3->3->4->4->5
-//输出: 1->2->5
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class deleteDuplicates {
     public static void main(String args[]) {
-        ListNode root=LinkUtil.create(new int[]{1,1,1,2,3,3,3,4,4,5});
+        ListNode root=LinkUtil.create(new int[]{1,2,3,3,4,4,5});
         root=deleteDuplicatesII(root);
         LinkUtil.disListNode(root);
     }
+    //输入: 1->2->3->3->4->4->5
+    //输出: 1->2->5
     public static ListNode deleteDuplicatesII(ListNode head){
         if (head==null||head.next==null)return head;
-        ListNode prev=new ListNode(),current=head;
-        while (current!=null){
-            int val=current.val;
-            while (current.val==val){
-                current=current.next;
-            }   
+        ListNode dummy=new ListNode(0);
+        ListNode prev=dummy,current=head,next=current.next,pointer=dummy;
+        while (current!=null&&next!=null){
+            if (current.val!=prev.val&&current.val!=next.val){
+                pointer.next=current;
+                pointer=pointer.next;
+            }
+            prev=current;
+            current=current.next;
+            next=next.next;
         }
-        return prev.next;
+        if (current.val!=prev.val)
+            pointer.next=current;
+        current.next=null;
+        return dummy.next;
     }
+    //输入: 1->1->2
+    //输出: 1->2
     public static ListNode deleteDuplicates(ListNode head) {
         if (head==null||head.next==null)return head;
-        ListNode prev=head,current=prev.next;
+        ListNode dummy=new ListNode(0,head);
+        ListNode prev=dummy.next,current=prev.next;
         while (current!=null){
             if (prev.val==current.val){
                 prev.next=current.next;
@@ -30,6 +44,6 @@ public class deleteDuplicates {
                 current=current.next;
             }
         }
-        return head;
+        return dummy.next;
     }
 }
