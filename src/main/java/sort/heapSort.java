06eb517919f13635fa75a5aa60d91e5f266efce7
fcy <807715333@qq.com>
2019-10-21 22:37:15 +0800

    '20191021223621'

--- src/main/java/sort/heapSort.java
+++ src/main/java/sort/heapSort.java
@@ -35,4 +35,11 @@ public class heapSort extends abstractSort{
             }
         }
     }
+    public static void bubbleSort(int[] array){
+        for (int i=0;i<array.length-1;i++){
+            for (int j=1;j<array.length-i;j++){
+                if (array[j])
+            }
+        }
+    }
 }
