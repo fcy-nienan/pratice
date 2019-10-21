bfacb2506abe23783aa6cc8c4703ecb1351eed56
fcy <807715333@qq.com>
2019-10-22 01:13:04 +0800

    '20191022'

--- src/main/java/sort/heapSort.java
+++ src/main/java/sort/heapSort.java
@@ -62,6 +62,7 @@ public class heapSort extends abstractSort{
             int j=i-1;
             while (j>=0&&value<array[j]){
                 array[j+1]=array[j];
+                j--;
             }
             array[j+1]=value;
         }
