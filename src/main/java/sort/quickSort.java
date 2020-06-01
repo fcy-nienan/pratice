a170c283214484eeed592312118f7097d5858e6c
807715333@qq.com <807715333@qq.com>
2020-06-01 20:09:46 +0800

    't'

--- src/main/java/sort/quickSort.java
+++ src/main/java/sort/quickSort.java
@@ -10,7 +10,7 @@ public class quickSort extends AbstractSort {
     public static void main(String args[]) {
         int[] arr1=new int[]{2,7,1,9,4,4,2};
 //        int[] arr=new int[]{2,7,1,9,4,4,3};
-        quickSort2(arr1,0,arr1.length-1);
+        quickSort3(arr1,0,arr1.length-1);
         System.out.println(Arrays.toString(arr1));
 //        new quickSort().arrayLen(10000).sortCount(1000).execute();
     }
@@ -41,7 +41,34 @@ public class quickSort extends AbstractSort {
             }
             sortUtil.swap(array,low,high);
         }
-        if (start!=low)quickSort2(array,start,low);
-        if (end!=high)quickSort2(array,low,end);
+        if (start!=low)quickSort2(array,start,low);//low-1 low栈溢出
+        if (end!=high)quickSort2(array,low+1,end);//直接low不加1  栈溢出
+    }
+
+
+
+    //快速排序
+    public static void quickSort3(int s[], int l, int r)
+    {
+        if (l < r)
+        {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j)
+            {
+                while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                if(i < j)
+                    s[i++] = s[j];
+
+                while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                if(i < j)
+                    s[j--] = s[i];
+            }
+            s[i] = x;
+            quickSort3(s, l, i - 1); // 递归调用
+            quickSort3(s, i + 1, r);
+        }
     }
 }
