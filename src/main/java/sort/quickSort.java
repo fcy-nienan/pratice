c8c15ea8c3b7e0d625a3ed1471626504d781d23a
807715333@qq.com <807715333@qq.com>
2020-06-01 19:43:46 +0800

    'sort'

--- src/main/java/sort/quickSort.java
+++ src/main/java/sort/quickSort.java
@@ -1,9 +1,18 @@
 package sort;
 
+import java.util.Arrays;
+
 public class quickSort extends AbstractSort {
+    @Override
+    protected void sort(int[] array) {
+        quickSort2(array,0,array.length-1);
+    }
     public static void main(String args[]) {
-//        new quickSort().execute();
-        new quickSort().arrayLen(10000).sortCount(1000).execute();
+        int[] arr1=new int[]{2,7,1,9,4,4,2};
+//        int[] arr=new int[]{2,7,1,9,4,4,3};
+        quickSort2(arr1,0,arr1.length-1);
+        System.out.println(Arrays.toString(arr1));
+//        new quickSort().arrayLen(10000).sortCount(1000).execute();
     }
     public static void quickSort(int[] array,int start,int end){
         int key=array[start];
@@ -12,39 +21,27 @@ public class quickSort extends AbstractSort {
             while (low<high&&array[high]>=key){//为什么需要等于?   2 .....  2
                 high--;
             }
-//            if (array[high]<key){
-//                sortUtil.swap(array,low,high);
-//            }
             while (low<high&&array[low]<key){//需要等于吗?   2 1
                 low++;
             }
-//            if (array[low]>key){
-                sortUtil.swap(array,low,high);
-//            }
+            sortUtil.swap(array,low,high);
         }
         if (low!=start)quickSort(array,start,low);
         if (high!=end)quickSort(array,low+1,end);
     }
-
-    @Override
-    protected void sort(int[] array) {
-        quickSort(array,0,array.length-1);
-    }
-    public static void quickSort1(int[] array,int low,int high){
-        int start=low,end=high;
-        int key=array[start];
-        while(start<end){
-            while (start<end&&array[end]>=key){
-                end--;
+    private static void quickSort2(int[] array,int start,int end){
+        int low=start,high=end;
+        int key=array[low];
+        while (low<high){
+            while (low<high&&array[high]>=key){
+                high--;
             }
-            sortUtil.swap(array,start,end);
-            while(start<end&&array[start]<=key){
-                start++;
+            while (low<high&&array[low]<key){
+                low++;
             }
-            sortUtil.swap(array,start,end);
+            sortUtil.swap(array,low,high);
         }
-        if (start!=low)quickSort1(array,low,start);
-        if (end!=high)quickSort1(array,start+1,high);
-
+        if (start!=low)quickSort2(array,start,low);
+        if (end!=high)quickSort2(array,low,end);
     }
 }
